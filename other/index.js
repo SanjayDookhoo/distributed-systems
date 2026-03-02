465d041202342b856e5f6aa7de0f330b6513b833
Sanjay Dookhoo <sanjaydookhoo@msn.com>
2026-03-02 14:35:58 -0400

    test

--- other/index.js
+++ other/index.js
@@ -15,42 +15,42 @@ const app = express()
 app.use(express.urlencoded({extended: true}))
 app.use(cors())
 
-// app.get("/photos", async (req, res) => {
-//   const albumId = req.query.albumId
-//   const { data } = await axios.get(
-//     "https://jsonplaceholder.typicode.com/photos",
-//     { params: { albumId } }
-//   )
-
-//   res.json(data)
-// })
-
-// app.get("/photos/:id", async (req, res) => {
-//   const { data } = await axios.get(
-//     `https://jsonplaceholder.typicode.com/photos/${req.params.id}`
-//   )
-
-//   res.json(data)
-// })
-
 app.get("/photos", async (req, res) => {
   const albumId = req.query.albumId
-  const key = `photos?albumId=${albumId}`;
-
-  const cached = await redisClient.get(key); // returns string | null
-  if (cached) {
-    console.log("Cache Hit")
-    return res.json(JSON.parse(cached));
-  }
-
-  // fetch, then cache
-  console.log("Cache Miss")
   const { data } = await axios.get(
     "https://jsonplaceholder.typicode.com/photos",
     { params: { albumId } }
   )
-  await redisClient.set(key, JSON.stringify(data), { expiration: {type: "EX", value: DEFAULT_EXPIRATION} });
-  return res.json(data);
+
+  res.json(data)
 })
 
-app.listen(3000)
+app.get("/photos/:id", async (req, res) => {
+  const { data } = await axios.get(
+    `https://jsonplaceholder.typicode.com/photos/${req.params.id}`
+  )
+
+  res.json(data)
+})
+
+// app.get("/photos", async (req, res) => {
+//   const albumId = req.query.albumId
+//   const key = `photos?albumId=${albumId}`;
+
+//   const cached = await redisClient.get(key); // returns string | null
+//   if (cached) {
+//     console.log("Cache Hit")
+//     return res.json(JSON.parse(cached));
+//   }
+
+//   // fetch, then cache
+//   console.log("Cache Miss")
+//   const { data } = await axios.get(
+//     "https://jsonplaceholder.typicode.com/photos",
+//     { params: { albumId } }
+//   )
+//   await redisClient.set(key, JSON.stringify(data), { expiration: {type: "EX", value: DEFAULT_EXPIRATION} });
+//   return res.json(data);
+// })
+
+app.listen(80)
